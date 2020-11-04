7426030ef6ce256df9bd9f5a284b09b88e885875
isys35 <isys35@mail.ru>
2020-11-04 22:12:34 +0300

    получение данных со слайдов

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import win32com.client
+import logging
 
-PPT_PATH = 'D:\Projects\commerce\parser_powerpoint\init_pptx\Fashion.pptx'
+PPT_PATH = 'D:\Project\commerce\parser_powerpoint\init_pptx\Fashion.pptx'
+
+logger = logging.getLogger("parser_loger")
+logger.setLevel(logging.INFO)
+console_headler = logging.StreamHandler()
+formatter = logging.Formatter('[%(levelname)s] %(asctime)s %(lineno)d : %(message)s')
+console_headler.setFormatter(formatter)
+logger.addHandler(console_headler)
 
 # Types 17 - Text box
 # Types 14 - Placeholder
+# Types 16 - Media
 
 
 class Slide:
@@ -12,29 +21,61 @@ class Slide:
         self.id = self.slide_com_object.SlideIndex
         self.texts = self.get_texts()
         self.images = self.get_pictures()
+        self.videos = self.get_videos()
+        self.background = self.get_background()
+        self.audio = self.get_audio()
+        self.time_slide = self.get_time_slide()
 
     def get_texts(self) -> list:
+        logger.info('Получение текстовой информации слайда №{}'.format(self.id))
         shapes = self.slide_com_object.Shapes
         text_frames = []
         for shape in shapes:
             if shape.Type == 17:
                 text_frame = shape.TextFrame
                 if text_frame.HasText:
-                    # print(text_frame.TextRange)
-                    # print(shape.Type)
-                    text_frames.append(SlideText(text_frame))
+                    text_frames.append(FrameText(text_frame))
         return text_frames
 
     def get_pictures(self) -> list:
+        logger.info('Получение картинок слайда №{}'.format(self.id))
         shapes = self.slide_com_object.Shapes
         images = []
         for shape in shapes:
             if shape.Type == 14:
-                print(shape.PictureFormat.Creator)
+                images.append(ShapeImage(shape))
+        return images
+
+    def get_videos(self):
+        logger.info('Получение видео слайда №{}'.format(self.id))
+        shapes = self.slide_com_object.Shapes
+        videos = []
+        for shape in shapes:
+            if shape.Type == 16:
+                if shape.MediaType == 3:
+                    videos.append(ShapeVideo(shape))
+        return videos
+
+    def get_audio(self):
+        logger.info('Получение аудио слайда №{}'.format(self.id))
+        shapes = self.slide_com_object.Shapes
+        audios = []
+        for shape in shapes:
+            if shape.Type == 16:
+                if shape.MediaType == 2:
+                    audios.append(ShapeAudio(shape))
+        return audios
+
+    def get_background(self):
+        logger.info('Получение фона слайда №{}'.format(self.id))
+        return self.slide_com_object.Background.Fill
+
+    def get_time_slide(self):
+        logger.info('Получение продолжительности слайда №{}'.format(self.id))
+        return self.slide_com_object.SlideShowTransition.Speed
 
 
-
-class SlideText:
+class FrameText:
     def __init__(self, text_frame_com_object):
         self.text_frame_com_object = text_frame_com_object
 
@@ -42,14 +83,36 @@ class SlideText:
         return self.text_frame_com_object.TextRange.Text
 
 
+class ShapeImage:
+    def __init__(self, shape_com_object):
+        self.shape_com_object = shape_com_object
+
+
+class ShapeVideo:
+    def __init__(self, shape_com_object):
+        self.shape_com_object = shape_com_object
+
+
+class FillBackground:
+    def __init__(self, fill_com_object):
+        self.fill_com_object = fill_com_object
+
+
+class ShapeAudio:
+    def __init__(self, shape_com_object):
+        self.shape_com_object = shape_com_object
+
+
 class PPT:
     def __init__(self, ppt_path):
+        logger.info('Открытие презентации {}'.format(ppt_path))
         self.app = win32com.client.Dispatch("Powerpoint.Application")
         self.ppt_path = ppt_path
         self.ppt_com_object = self.app.Presentations.Open(self.ppt_path)
         self.slides = self.get_slides()
 
     def get_slides(self) -> list:
+        logger.info('Получение слайдов')
         return [Slide(slide_com_object) for slide_com_object in self.ppt_com_object.Slides]
 
     def close(self):
@@ -72,6 +135,7 @@ def main():
     #
     #     print('')
     # ppt_app.Quit()
+
 
 if __name__ == '__main__':
     main()
